68d7d79b213137f35bee4f15859a1d93b32434ce
Jenna Mitchell <99043514+JennaMitchell@users.noreply.github.com>
2023-08-30 18:22:21 -0400

    removed console.log

--- index.js
+++ index.js
@@ -9,19 +9,15 @@ const recipeRoute = require("./routes/recipe-routes");
 const authRoutes = require("./routes/auth");
 
 const cors = require("cors");
-console.log(12);
+
 mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("Connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-console.log(22);
+  .then(() => {})
+  .catch((err) => {});
+
 app.use(bodyParser.json());
-console.log(34);
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -31,7 +27,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 });
-console.log(34);
+
 app.use(cors());
 app.use(express.json());
 app.use("/recipes", recipeRoute);
